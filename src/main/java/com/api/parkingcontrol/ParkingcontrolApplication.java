31b9f45fadb76fc77ed43be32ab5c95741b85d88
Amanda Neves <amandaneves.carmo@gmail.com>
2022-08-04 00:55:24 -0300

    Adicionando autenticação em BD via JPA.

--- src/main/java/com/api/parkingcontrol/ParkingcontrolApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingcontrolApplication.java
@@ -2,6 +2,7 @@ package com.api.parkingcontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,8 @@ public class ParkingcontrolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingcontrolApplication.class, args);
+
+		// System.out.println(new BCryptPasswordEncoder().encode("a"));
 	}
 
 	@GetMapping("/")
--- src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
@@ -1,11 +1,20 @@
 package com.api.parkingcontrol.configs.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+// @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -16,5 +25,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // auth.inMemoryAuthentication()
+        //        .withUser("amanda")
+        //        .password(passwordEncoder().encode("102030"))
+        //        .roles("ADMIN");
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
