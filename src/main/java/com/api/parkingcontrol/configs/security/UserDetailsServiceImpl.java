0f3204c25ac976269d3b64f9c47387fffceca552
Amanda Neves <amandaneves.carmo@gmail.com>
2022-08-07 18:19:17 -0300

    Adicionando Controle de Acesso.

--- src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
+++ src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
@@ -3,12 +3,15 @@ package com.api.parkingcontrol.configs.security;
 import com.api.parkingcontrol.models.UserModel;
 import com.api.parkingcontrol.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
@@ -19,6 +22,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         UserModel userModel = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
 
-        return userModel;
+        return new User(userModel.getUsername(), userModel.getPassword(), true, true, true, true, userModel.getAuthorities());
     }
 }
